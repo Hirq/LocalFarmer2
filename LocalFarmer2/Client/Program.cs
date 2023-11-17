802dad0fc1a62fd748e82ad906cd26f1366ba638
Cezary Zabrocki <cezary.zabrocki@sharpsoft.pl>
2023-11-17 14:54:51 +0100

    Mud Blazor - now I must verify product and farmhouse

--- LocalFarmer2/Client/Program.cs
+++ LocalFarmer2/Client/Program.cs
@@ -5,15 +5,19 @@ using LocalFarmer2.Client.Utilities;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddSingleton<AlertService>();
+builder.Services.AddSingleton<UtilsService>();
 
 
 await builder.Build().RunAsync();
