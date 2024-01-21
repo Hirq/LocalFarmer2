abd25d1bb154279c84aa2474ecab19a576171a10
Zabrocki-PC <czarkooo@gmail.com>
2024-01-21 19:06:57 +0100

    add farmhouse with marker coordinals

--- LocalFarmer2/Shared/DTOs/AddFarmhouseDto.cs
+++ LocalFarmer2/Shared/DTOs/AddFarmhouseDto.cs
@@ -6,8 +6,8 @@
         public string Description { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
         public string IdUser { get; set; }
     }
 }
