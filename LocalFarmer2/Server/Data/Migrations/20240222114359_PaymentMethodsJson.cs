b3d7e67b06fadd5189eebe4a689cd286b2229d39
Zabrocki-PC <czarkooo@gmail.com>
2024-02-22 12:43:29 +0100

    Add Json for payment methods

---
+++ LocalFarmer2/Server/Data/Migrations/20240222114359_PaymentMethodsJson.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalFarmer2.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentMethodsJson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethodsJson",
+                table: "Farmhouses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethodsJson",
+                table: "Farmhouses");
+        }
+    }
+}
