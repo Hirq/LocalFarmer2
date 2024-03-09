408379ebaa57066c954e1695a821b1996ec0bb27
Zabrocki-PC <czarkooo@gmail.com>
2024-03-09 08:34:00 +0100

    add model Alert

---
+++ LocalFarmer2/Shared/Models/Alert.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LocalFarmer2.Shared.Models
+{
+    public class Alert
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string IdUser { get; set; }
+
+        [ForeignKey(nameof(Farmhouse))]
+        public int IdFarmhouse { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public Farmhouse Farmhouse { get; set; }
+    }
+}
