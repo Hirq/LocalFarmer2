ccca95cfe513594f4c9c0d714a3b0c26bcf7e5f6
Zabrocki-PC <czarkooo@gmail.com>
2024-04-06 19:58:24 +0200

    add new alert

--- LocalFarmer2/Shared/ENUMs/MessageAlert.cs
+++ LocalFarmer2/Shared/ENUMs/MessageAlert.cs
@@ -3,19 +3,28 @@
     public enum MessageAlertEnum
     {
         MessageIsOpen,
+        NewProduct,
+        EditProduct,
+        EditDetails,
+        //
+        NewSubscriber,
+        NewOpinion,
+        //NewMessageFromForm
     }
 
     public class MessageAlert
     {
-        public string Name { get; }
-        public bool IsOpen { get; }
+        public string Value { get; }
+        public string? Value2 { get; }
+        public bool? IsOpen { get; }
         public MessageAlertEnum AlertEnum { get; }
 
-        public MessageAlert(MessageAlertEnum alertEnum, string nameFarmhouse, bool isOpenFarmhouse)
+        public MessageAlert(MessageAlertEnum alertEnum, string value, string? value2 = null, bool? isOpen = null)
         {
             AlertEnum = alertEnum; 
-            Name = nameFarmhouse;
-            IsOpen = isOpenFarmhouse;
+            Value = value;
+            Value2 = value2;
+            IsOpen = isOpen;
         }
 
         public string GetMessage()
@@ -23,7 +32,17 @@
             switch (AlertEnum)
             {
                 case MessageAlertEnum.MessageIsOpen:
-                    return $"Farmhouse {Name} is actually {(IsOpen ? "open" : "close")}";
+                    return $"Farmhouse {Value} is actually {((bool)IsOpen ? "open" : "close")}";
+                case MessageAlertEnum.NewProduct:
+                    return $"Farmhouse {Value} add new product {Value2}";
+                case MessageAlertEnum.EditProduct:
+                    return $"Farmhouse {Value} edit product {Value2}";
+                case MessageAlertEnum.EditDetails:
+                    return $"Farmhouse {Value} change information";
+                case MessageAlertEnum.NewSubscriber:
+                    return $"New Subscriber {Value}";
+                case MessageAlertEnum.NewOpinion:
+                    return $"New Opinion {Value}";
                 default:
                     return string.Empty;
             }
