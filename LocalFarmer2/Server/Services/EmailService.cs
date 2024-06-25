63de878dcc48e7df34802fc262d07308119ed751
Zabrocki-PC <czarkooo@gmail.com>
2024-06-25 13:26:42 +0200

    email cd

--- LocalFarmer2/Server/Services/EmailService.cs
+++ LocalFarmer2/Server/Services/EmailService.cs
@@ -20,7 +20,15 @@ namespace LocalFarmer2.Server.Services
             email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
             email.To.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
             email.Subject = $"LocalFarmer: {request.Subject}";
-            email.Body = new TextPart(TextFormat.Html) { Text = $"{request.From} <br /> <br />  {request.Body} <br /> <br />  Send from application LocalFarmer " };
+            email.Body = new TextPart(TextFormat.Html) 
+            { 
+                Text = 
+                    $"{request.From}" +
+                    $"<br /> <br />" +
+                    $"{request.Body}" +
+                    $" <br /> <br />" +
+                    $"Send from application LocalFarmer"
+            };
 
             using var smtp = new SmtpClient();
             smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
