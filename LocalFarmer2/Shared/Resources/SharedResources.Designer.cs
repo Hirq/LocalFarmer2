7bb03663a24850974c5306a7a53c1ea6b229c3ce
Zabrocki-PC <czarkooo@gmail.com>
2024-08-12 16:53:02 +0200

    now favorite farmhouses

--- LocalFarmer2/Shared/Resources/SharedResources.Designer.cs
+++ LocalFarmer2/Shared/Resources/SharedResources.Designer.cs
@@ -22,7 +22,7 @@ namespace LocalFarmer2.Shared.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class SharedResources {
+    public class SharedResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
