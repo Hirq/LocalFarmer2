ffdbc96ae9e01b464db00138300a7384c68b9153
Zabrocki-PC <czarkooo@gmail.com>
2024-09-29 12:40:24 +0200

    notes css

---
+++ LocalFarmer2/Shared/ViewModels/NoteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalFarmer2.Shared.ViewModels
+{
+    public class NoteViewModel
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public bool IsFlipped { get; set; } = false;
+    }
+}
