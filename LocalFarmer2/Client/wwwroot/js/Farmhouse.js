93ff769f78efdb5be0a970bea4b5e614fb62a0e8
Zabrocki-PC <czarkooo@gmail.com>
2024-10-11 23:14:37 +0200

    Text for note now its ok

--- LocalFarmer2/Client/wwwroot/js/Farmhouse.js
+++ LocalFarmer2/Client/wwwroot/js/Farmhouse.js
@@ -6,3 +6,23 @@ function closeAlertAfterDelay() {
         }
     }, 2500);
 }
+
+function checkScrolls() {
+    // Pobieramy wszystkie elementy z klas¹ noteText
+    var elements = document.querySelectorAll('.noteText');
+
+elements.forEach(function (element) {
+        // Sprawdzamy, czy element ma aktywny scrollbar
+        if (element.scrollHeight > element.clientHeight) {
+    // Je¿eli jest aktywny scrollbar, ustaw margin-left na 10px
+    element.style.marginLeft = '10px';
+        } else {
+    // Je¿eli nie ma scrollbara, ustaw margin-left na 0px
+    element.style.marginLeft = '0px';
+        }
+    });
+}
+
+// Sprawdzamy na za³adowaniu oraz przy ka¿dej zmianie rozmiaru okna
+window.onload = checkScrolls;
+window.onresize = checkScrolls;
