5fbd1db066d79e28beed5724188207336acde84b
Zabrocki-PC <czarkooo@gmail.com>
2025-01-17 19:20:19 +0100

    Prepared vision how used invidual chat

--- LocalFarmer2/Server/Hubs/ChatHub.cs
+++ LocalFarmer2/Server/Hubs/ChatHub.cs
@@ -4,8 +4,24 @@ namespace LocalFarmer2.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        public ChatHub()
+        {
+               
+        }
+
         public async Task SendMessage(string user, string message)
         {
+            string senderId = "";
+            string receiverId = "";
+
+            List<string> users = new List<string>()
+            {
+                senderId,
+                receiverId
+            };
+
+            
+            await Clients.Users(users).SendAsync("ReceiveMessage", user, message);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
     }
