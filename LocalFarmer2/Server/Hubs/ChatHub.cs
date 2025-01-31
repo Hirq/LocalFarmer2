2abf1f68b661bf8199b29c78b7fb8f2b7c03688c
Zabrocki-PC <czarkooo@gmail.com>
2025-01-31 12:23:22 +0100

    chat work for 2 people

--- LocalFarmer2/Server/Hubs/ChatHub.cs
+++ LocalFarmer2/Server/Hubs/ChatHub.cs
@@ -13,7 +13,7 @@ namespace LocalFarmer2.Server.Hubs
 
         public async Task SendMessage(ChatMessageDto dto)
         {
-            Console.WriteLine($"[ChatHub] Wysyłam wiadomość od: {dto.IdUserSender} do: {dto.IdUserReceiver}");
+            await Groups.AddToGroupAsync(Context.ConnectionId, dto.IdUserSender);
 
             var message = await _chatMessageService.SendMessage(dto);
             //var date = DateTime.Now.ToShortTimeString();
@@ -25,14 +25,9 @@ namespace LocalFarmer2.Server.Hubs
                 dto.IdUserReceiver
             };
 
-            //List<string> users2 = new List<string>()
-            //{
-            //    "53898801-370c-4fbc-a99c-86243f159fe5",
-            //    "8e339c44-2ccf-4e17-b774-626a7c9c8c1b"
-            //};
-
-            await Clients.Users(users).SendAsync("ReceiveMessage", dto);
-            //await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.Group(dto.IdUserReceiver).SendAsync("ReceiveMessage", dto);
+            await Clients.Group(dto.IdUserSender).SendAsync("ReceiveMessage", dto);
+            //await Clients.All.SendAsync("ReceiveMessage", dto);
         }
     }
 }
