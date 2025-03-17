78d2388c6cf3e0b5e21929248b48117f531c4ee7
Zabrocki-PC <czarkooo@gmail.com>
2025-03-17 19:58:46 +0100

    styles

--- LocalFarmer2/Client/wwwroot/js/chat.js
+++ LocalFarmer2/Client/wwwroot/js/chat.js
@@ -1,16 +1,13 @@
 ﻿export function downloadFile(mimeType, base64String, fileName) {
-
     var fileDataUrl = "data:" + mimeType + ";base64," + base64String;
     fetch(fileDataUrl)
         .then(response => response.blob())
         .then(blob => {
 
-            //create a link
             var link = window.document.createElement("a");
             link.href = window.URL.createObjectURL(blob, { type: mimeType });
             link.download = fileName;
 
-            //add, click and remove
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
@@ -19,13 +16,18 @@
 }
 
 export function setScroll() {
-    //let's fix scroll here
-    var divMessageContainerBase = document.getElementById('divMessageContainerBase');
-    if (divMessageContainerBase != null) {
-        divMessageContainerBase.scrollTop = divMessageContainerBase.scrollHeight;
+    var divMessageContainer = document.getElementById('mud-paper-chat-message');
+    if (divMessageContainer != null) {
+        divMessageContainer.scrollTop = divMessageContainer.scrollHeight;
     }
 }
 
+export function scrollOnBottomMessages() {
+    var container = document.getElementById("mud-paper-chat-message");
+    if (container) {
+        container.scrollTop = container.scrollHeight;
+    }
+}
 
 document.getElementById("txtMessageInput")
     .addEventListener("keyup", function(event) {
