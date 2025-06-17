2295f76fe32f4d29827a8b67b019367eb35715f1
Zabrocki-PC <czarkooo@gmail.com>
2025-06-17 20:33:00 +0200

    Add faq

---
+++ LocalFarmer2/Shared/Utilities/FaqItem.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalFarmer2.Shared.Utilities
+{
+    public class FaqItem
+    {
+        public string Id { get; set; }
+        public Dictionary<string, string> Question { get; set; }
+        public Dictionary<string, string> Answer { get; set; }
+    }
+}
