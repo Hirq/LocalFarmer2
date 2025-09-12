6612abf22d284a2f6297811c71f86cfefb7b877d
Zabrocki <czarkooo@gmail.com>
2025-09-12 13:54:19 +0200

    BasePage for all razor pages

--- LocalFarmer2/Client/Pages/BasePage.cs
+++ LocalFarmer2/Client/Pages/BasePage.cs
@@ -1,12 +1,19 @@
+using LocalFarmer2.Client.Services;
 using Microsoft.AspNetCore.Components;
-
 
 namespace LocalFarmer2.Client.Pages
 {
     public abstract class BasePage : ComponentBase
     {
+        [Inject] protected IAccountService AccountService { get; set; }
+        [Inject] protected UserStateService UserStateService { get; set; }
+        [Inject] protected IStringLocalizer<SharedResources> Loc { get; set; }
+        protected virtual string NotLoggedInMessage => Loc?["ErrorUserIsNotLogged"] ?? "User is not logged in.";
+        protected virtual string MissingFarmhouseMessage => Loc?["ErrorUserDoesnotHaveFarmhouse"] ?? "User does not have farmhouse.";
+
         protected bool isLoading = false;
         protected string? errorMessage;
+        public bool HasError => !string.IsNullOrEmpty(errorMessage);
 
         protected override async Task OnInitializedAsync()
         {
@@ -27,5 +34,36 @@ namespace LocalFarmer2.Client.Pages
             }
         }
         protected abstract Task LoadDataAsync();
+
+        protected async Task<bool> EnsureLoggedInAsync()
+        {
+            var isLogged = await AccountService.IsUserLogged();
+            if (!isLogged)
+            {
+                errorMessage = NotLoggedInMessage;
+                return false;
+            }
+
+            var user = await AccountService.GetCurrentUser();
+            if (user == null)
+            {
+                errorMessage = NotLoggedInMessage;
+                return false;
+            }
+
+            UserStateService.CurrentUser = user;
+            return true;
+        }
+
+        protected async Task<bool> EnsureHasFarmhouseAsync()
+        {
+            var user = UserStateService.CurrentUser ?? await AccountService.GetCurrentUser();
+            if (user == null || user.IdFarmhouse == null)
+            {
+                errorMessage = MissingFarmhouseMessage;
+                return false;
+            }
+            return true;
+        }
     }
 }
